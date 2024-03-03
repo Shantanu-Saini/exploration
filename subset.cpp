d1e7b14de9ce06db61c9c74f2d261386b9796d7a
Shantanu <shantanusaini7000@gmail.com>
2024-03-04 01:55:56 +0530

    subset arrays

---
+++ subset.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m);
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int a1[n], a2[m];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a1[i];
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> a2[i];
+        }
+
+        cout << isSubset(a1, a2, n, m) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+string isSubset(int a1[], int a2[], int n, int m)
+{
+
+    bool flag = false;
+    for (int i = 0; i < m; i++)
+    {
+        flag = false;
+        for (int j = 0; j < n; j++)
+        {
+            if (a2[i] == a1[j])
+            {
+                a1[j] = 0;
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false)
+        {
+            return "No";
+        }
+    }
+    return "Yes";
+}
+
+/*
+Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m, where both arrays may contain duplicate elements. The task is to determine whether array a2 is a subset of array a1. It's important to note that both arrays can be sorted or unsorted. Additionally, each occurrence of a duplicate element within an array is considered as a separate element of the set.
+
+Example 1:
+
+Input:
+a1[] = {11, 7, 1, 13, 21, 3, 7, 3}
+a2[] = {11, 3, 7, 1, 7}
+Output:
+Yes
+Explanation:
+a2[] is a subset of a1[]
+*/
